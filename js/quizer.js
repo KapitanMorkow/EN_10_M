87a0175a8587f09539507199846e3ce3ab4fa83a
KapitanMorkow <157925142+KapitanMorkow@users.noreply.github.com>
2026-01-22 01:24:36 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -1105,9 +1105,14 @@ let en_2010_m = [
 		song : "PILLOWTALK (2016)"
 	},
 	{
-		pack : EN_2020_M_PACK_4,
+		pack : EN_2010_M_PACK_4,
 		group : 'Rema',
 		song : "Calm Down (2022)"
+	},
+	{
+		pack : EN_2010_M_PACK_4,
+		group : 'Shaboozey',
+		song : "A Bar Song (Tipsy) (2024)"
 	}
 ];
 
